632456354d4319631639a57c1bffe429a4551a55
Bhavesh Ranjan <75161205+bhaveshranjan@users.noreply.github.com>
2023-04-18 13:45:52 +0530

    This project is a random password generator developed using HTML, CSS, and JavaScript. The purpose of this project is to provide users with a tool that generates a secure and randomized password, which can be used for different online accounts. The generator allows users to select the length of the password, as well as the types of characters to include, such as uppercase and lowercase letters, numbers, and special characters.

--- Random Password Generator/script.js
+++ Random Password Generator/script.js
@@ -14,7 +14,7 @@ const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 
-//initially
+
 let password = "";
 let passwordLength = 10;
 let checkCount = 0;
